ae4a9b2c38577cca8770d17891dd1faa61288be9
Zhanar <sadykova.zhanar@gmail.com>
2018-05-29 17:09:22 +0600

    Add project files.

---
+++ Module2_Lesson2_Home_task/Source.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<math.h>
+
+void main()
+{
+	int n = 0;
+
+	printf("Введите номер задания:\t");
+	scanf("%d", &n);
+
+	if (n == 1)
+	{
+
+	}
+
+
+}
